85b51a89f227232371f10e14f3b92c49ebba41f3
Tomasz Gieryn <Tomasz.Gieryn@gft.com>
2018-11-26 15:17:17 +0100

    Initialized git repo.

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { ListViewComponent } from './list-view/list-view.component';
+import { ListItemComponent } from './list-item/list-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListViewComponent,
+    ListItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
